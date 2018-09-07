666d088c49b27a217075f0a220c5b8a04ddcf7c5
CB6 <melissa.j.womack@gmail.com>
2018-09-07 15:26:20 -0500

    Added hammer.js, fixed about and contact nav on home page

--- javascript/index.js
+++ javascript/index.js
@@ -1,24 +1,63 @@
 M.AutoInit();
-$(document).ready(function () {
 
-    $('.slider').slider({
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.slider');
+    var instance = M.Slider.init(elems, {
         indicators: true,
         interval: 4000
+
     });
 
-    $('.indicator-item').on( "click", function() {
-        $('.slider').slider('pause')
-      });
+    $(".aboutme").on("click", function (event) {
+        var index = $(".slider").find('.active').index();
+        var name = $(".name").index();
+        var about = $(".about").index();
+        var contact = $(".contact").index();
+
+        switch(index) {
+            case name:
+            $('.slider').slider('next');
+            break;
+            case about:
+            $('.slider').slider('pause');
+            break;
+            case contact:
+            $('.slider').slider('prev');
+            break;
+        }
+    });
+
+    $(".contactme").on("click", function (event) {
+        var index = $(".slider").find('.active').index();
+        var name = $(".name").index();
+        var about = $(".about").index();
+        var contact = $(".contact").index();
+
+        switch(index) {
+            case name:
+            $('.slider').slider('prev');
+            break;
+            case about:
+            $('.slider').slider('next');
+            break;
+            case contact:
+            $('.slider').slider('pause');
+            break;
+        }
+    });
+
 });
 
-(function($){
-    $(function(){
-  
-      $('.sidenav').sidenav();
-      $('.parallax').parallax();
-  
-    }); // end of document ready
-  })(jQuery);
+
+
+(function ($) {
+    $(function () {
+
+        $('.sidenav').sidenav();
+        $('.parallax').parallax();
+
+    });
+})(jQuery);
 
 
 
