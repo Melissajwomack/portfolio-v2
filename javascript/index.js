ac62056cd1f5e93b678edcead64761ababef58af
CB6 <melissa.j.womack@gmail.com>
2018-09-12 19:02:13 -0500

    Commented and cleaned up code

--- javascript/index.js
+++ javascript/index.js
@@ -1,24 +1,33 @@
+//Initialize materialize javascript elements
 M.AutoInit();
 
+//Wait for document to load
 document.addEventListener("DOMContentLoaded", function () {
+    //Initialize slider on index page
     var elems = document.querySelectorAll(".slider");
     var instance = M.Slider.init(elems, {
+        //slider options
         indicators: false,
         interval: 20000
 
     });
 
+    //Initialize sidenav
     var elems = document.querySelectorAll(".sidenav");
     var instances = M.Sidenav.init(elems, {
+        //sidenave options
         draggable: true
     });
 
+    //Initialize parallax
     $(".parallax").parallax();
 
+    //Pause slider function
     function pause() {
         $(".slider").slider("pause");
     }
 
+    //Function to make slider go to contact me slide
     function goToContact() {
         var index = $(".slider").find(".active").index();
         var name = $(".name").index();
@@ -40,6 +49,7 @@ document.addEventListener("DOMContentLoaded", function () {
         };
     };
 
+    //Function to make slider go to about me slide
     function goToAbout() {
         var index = $(".slider").find(".active").index();
         var name = $(".name").index();
@@ -61,16 +71,19 @@ document.addEventListener("DOMContentLoaded", function () {
         };
     };
 
+    //On click event for nav bar that brings user to about me slide
     $(".aboutme").on("click", function () {
         goToAbout();
         $(".sidenav").sidenav("close");
     });
 
+    //On click event for nav bar that brings user to contact me slide
     $(".contactme").on("click", function () {
         goToContact();
         $(".sidenav").sidenav("close");
     });
 
+    //On click event for nav bar that opens PDF of resume
     $(".resume").on("click", function() {
         $(".sidenav").sidenav("close");
     })
